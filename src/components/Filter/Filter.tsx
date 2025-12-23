b466e2450cab67ce8c18bdc3fa9711b864c55962
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-12-24 01:16:30 +0500

    Delete elements and styles

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,13 +1,36 @@
+'use client'
+
 import styles from './filter.module.css';
+import FilterItem from '../FilterItem/FilterItem';
+import { useState } from 'react';
 
 
 export default function Filter() {
+  const [isOpen, setIsOpen] = useState("");
+
+  const onOpenDropdownList = (title: string) => {
+    setIsOpen(title === isOpen ? "" : title); // закрыть список, если он уже открыт
+    console.log("Открыть список: ", title);
+  };
+
   return (
     <div className={styles.centerblock__filter}>
       <div className={styles.filter__title}>Искать по:</div>
-      <div className={styles.filter__button}>исполнителю</div>
-      <div className={styles.filter__button}>году выпуска</div>
-      <div className={styles.filter__button}>жанру</div>
+      <FilterItem
+        title="исполнителю"
+        onClick={() => onOpenDropdownList("исполнителю")}
+        isOpen={isOpen === "исполнителю"}
+        />
+      <FilterItem
+        title="году выпуска"
+        onClick={() => onOpenDropdownList("году выпуска")}
+        isOpen={isOpen === "году выпуска"}
+        />
+      <FilterItem
+        title="жанру"
+        onClick={() => onOpenDropdownList("жанру")}
+        isOpen={isOpen === "жанру"}
+      />
     </div>
   )
 }
