d36071f67935facdff4c7fbb2589daecb3e13582
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-02-07 00:00:49 +0500

    Fix displaying favorite tracks after refresh page

--- src/app/music/favorite/page.tsx
+++ src/app/music/favorite/page.tsx
@@ -2,30 +2,46 @@
 
 import Centerblock from '@/components/Centerblock/Centerblock';
 import { TrackType } from '@/sharedTypes/sharedTypes';
-import { useAppSelector } from '@/store/store';
+import { setFavoriteTracks } from '@/store/features/trackSlice';
+import { useAppDispatch, useAppSelector } from '@/store/store';
 import { useEffect, useState } from 'react';
 
 
 export default function FavoritePage() {
-  const { favoriteTracks, fetchIsLoading, fetchError,  allTracks, filters, filtredTracks } = useAppSelector((state) => state.tracks);
+  const dispatch = useAppDispatch();
+  // const { favoriteTracks, fetchIsLoading, fetchError, allTracks, filters, filtredTracks } = useAppSelector((state) => state.tracks);
+  const { fetchIsLoading, fetchError, filters, filtredTracks } = useAppSelector((state) => state.tracks);
 
   const isAuthRequired = true;
 
   // получить плэйлист текущей страницы
   const [playlist, setPlaylist] = useState<TrackType[]>([]);
+  const [myTracks, setMyTracks] = useState<TrackType[]>([]);
+
+  useEffect(() => {
+    const savedFavorites = localStorage.getItem('favoriteTracks');
+    // console.log("savedFavorites: ", savedFavorites);
+
+    const favoritePlaylist: TrackType[] | [] | null = savedFavorites ? JSON.parse(savedFavorites) : [];
+    // console.log("favoritePlaylist: ", favoritePlaylist);
+
+    if (favoritePlaylist) {
+      setMyTracks(favoritePlaylist);
+    }
+  }, []);
 
   // получить плэйлист текущей страницы в зависимости от ипользования фильтров, поиска
   useEffect(() => {
-    const currentPlaylist = filters.authors.length ? filtredTracks : favoriteTracks;
+    const currentPlaylist = filters.authors.length ? filtredTracks : myTracks;
     setPlaylist(currentPlaylist);
-  }, [favoriteTracks, filtredTracks]);
+  }, [myTracks, filtredTracks]);
 
 
   return (
     <>
       <Centerblock
         categoryName="Мои треки"
-        pagePlaylist={favoriteTracks}
+        pagePlaylist={myTracks}
         // playlist={favoriteTracks}
         playlist={playlist}
         isLoading={fetchIsLoading}
