678528f1f32c4700a1347c8fc648c41e3b13af12
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-02-06 10:22:07 +0500

    Add filtering, sorting on favirite tracks page

--- src/app/music/favorite/page.tsx
+++ src/app/music/favorite/page.tsx
@@ -1,19 +1,33 @@
 'use client';
 
 import Centerblock from '@/components/Centerblock/Centerblock';
+import { TrackType } from '@/sharedTypes/sharedTypes';
 import { useAppSelector } from '@/store/store';
+import { useEffect, useState } from 'react';
 
 
 export default function FavoritePage() {
-  const { favoriteTracks, fetchIsLoading, fetchError } = useAppSelector((state) => state.tracks);
+  const { favoriteTracks, fetchIsLoading, fetchError,  allTracks, filters, filtredTracks } = useAppSelector((state) => state.tracks);
 
   const isAuthRequired = true;
+
+  // получить плэйлист текущей страницы
+  const [playlist, setPlaylist] = useState<TrackType[]>([]);
+
+  // получить плэйлист текущей страницы в зависимости от ипользования фильтров, поиска
+  useEffect(() => {
+    const currentPlaylist = filters.authors.length ? filtredTracks : favoriteTracks;
+    setPlaylist(currentPlaylist);
+  }, [favoriteTracks, filtredTracks]);
+
 
   return (
     <>
       <Centerblock
         categoryName="Мои треки"
-        playlist={favoriteTracks}
+        pagePlaylist={favoriteTracks}
+        // playlist={favoriteTracks}
+        playlist={playlist}
         isLoading={fetchIsLoading}
         error={fetchError || ''}
         isAuthRequired={isAuthRequired}
