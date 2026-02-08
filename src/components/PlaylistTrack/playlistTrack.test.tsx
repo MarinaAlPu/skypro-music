c537eee4217088b5ae931cba7526769bbf684d54
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-02-08 22:59:41 +0700

    Add tests for component PlaylistTrack

--- src/components/PlaylistTrack/playlistTrack.test.tsx
+++ src/components/PlaylistTrack/playlistTrack.test.tsx
@@ -7,10 +7,34 @@ import { TrackType } from "@/sharedTypes/sharedTypes";
 import ReduxProvider from '@/store/ReduxProvider';
 import PlaylistTrack from './PlaylistTrack';
 import { formatTime } from '@/utils/helpers';
+import userEvent from '@testing-library/user-event';
 
 
 const mockTracks: TrackType[] = data;
 const mockTrack: TrackType = data[0];
+
+// Мокирование useRouter из next/navigation
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(),
+}));
+
+
+// создать мок для router.push
+const mockPush = jest.fn();
+const mockUseRouter = require('next/navigation').useRouter;
+
+
+beforeEach(() => {
+  // настроить мок перед каждым тестом
+  mockUseRouter.mockReturnValue({
+    push: mockPush,
+  });
+});
+
+afterEach(() => {
+  // очистить все моки после каждого теста
+  jest.clearAllMocks();
+});
 
 
 describe('PlaylistTrack component', () => {
@@ -22,6 +46,52 @@ describe('PlaylistTrack component', () => {
     )
     expect(screen.getAllByText('Chase').length).toBeGreaterThan(0);
     expect(screen.getAllByText(mockTrack.name).length).toBeGreaterThan(0);
+    expect(screen.getAllByText(mockTrack.author).length).toBeGreaterThan(0);
+    expect(screen.getAllByText(mockTrack.album).length).toBeGreaterThan(0);
     expect(screen.getAllByText(formatTime(mockTrack.duration_in_seconds)).length).toBeGreaterThan(0);
+  });
+
+  test('После клика по треку отображается иконка, что трек выбран', async () => {
+    const user = userEvent.setup();
+
+    const { container } = render(
+      <ReduxProvider>
+        <PlaylistTrack track={mockTrack} playlist={mockTracks} />
+      </ReduxProvider>
+    );
+
+    const trackTitle = container.querySelector('div.track__title');
+
+    if (trackTitle) {
+      await user.click(trackTitle);
+
+      const selectedTrackIcon = container.querySelector('svg.track__selected');
+
+      expect(selectedTrackIcon).toBeInTheDocument();
+      expect(selectedTrackIcon).toBeVisible();
+    } else {
+      throw new Error('Названий треков не обнаружено');
+    }
+  });
+
+  test('Без авторизации не ставится лайк', async () => {
+    const user = userEvent.setup();
+
+    const { container } = render(
+      <ReduxProvider>
+        <PlaylistTrack track={mockTrack} playlist={mockTracks} />
+      </ReduxProvider>
+    );
+
+    const likeButton = container.querySelector('svg.track__timeSvg > use');
+
+    if (likeButton) {
+      await user.click(likeButton);
+
+      expect(likeButton).toHaveAttribute('xlink:href', '/img/icon/sprite.svg#icon-like');
+      expect(likeButton).not.toHaveAttribute('xlink:href', '/img/icon/sprite.svg#icon-like-active');
+    } else {
+      throw new Error('Названий треков не обнаружено');
+    }
   });
 });
